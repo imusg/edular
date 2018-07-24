eefd9ee343ebda43e76af2acdb45f6492e0cc3a3
Maksim <gozhev.m@megamix.ru>
2018-07-24 09:19:52 +0300

    Первый коммит

---
+++ resources/views/home.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row header">
+        <div class="col-md-12">
+            <div id="top_menu">
+
+                <nav>
+                    <ul class="nav nav-tabs" id="top_menu_items">
+                        <li class="nav-item">
+                            <a href="{{ url('/profile') }}" class="nav-link active" onclick="" id="menu_home">Главная</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Все клиенты</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="{{ url('/counterpartiesView?date=DATE_CREATE&sort=DESC&page=0') }}" id="all_counterparties">Контрагенты</a>
+                                <a class="dropdown-item" href="#" onclick="leads_view('DATE_CREATE','DESC')"  id="all_leads">Лиды</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Персональные отчеты
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" onclick='getReportActOrderShip()'>Актуальные заявки на поставку</a>
+                                <a class="dropdown-item" href="#" onclick='getReportPretenzii()'>Жалобы претензии</a>
+                                <a class="dropdown-item" href="#" onclick='getReportRunOrder()'>Исполнение заявок</a>
+                                <a class="dropdown-item" href="#" onclick='getReportSalesOnAnimal()'>Продажи по видам животных</a>
+                                <a class="dropdown-item" href="#" onclick='getReportAnalPlanMeneger()'>Анализ планов менеджера</a>
+                                <a class="dropdown-item" href="#" onclick='getReportNoWorkPlan()'>Не обработанные планы менеджера</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Общие отчеты
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" onclick='report_plan_mf()'>План производства</a>
+                                <a class="dropdown-item" href="#" onclick='report_manager_plan_mf()'>План менеджера на производства</a>
+                                <a class="dropdown-item" href="#" onclick='getReportDispatch()'>Текущие отгрузки</a>
+                                <a class="dropdown-item" href="#" onclick='getAnalProdSklad()'>Анализ доступности товаров</a>
+
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+
+                <!--<div class="tabbar tabbar&#45;&#45;top tabbar&#45;&#45;material">-->
+                <!--<label class="tabbar__item tabbar&#45;&#45;material__item" onclick="location.href('https://bxapp.megamix.ru/megacrm/index.html')" id="menu_home">-->
+                <!--<input type="radio" name="tabbar-material-a" checked="checked">-->
+                <!--<button class="tabbar__button tabbar&#45;&#45;material__button">-->
+                <!--Главная-->
+                <!--</button>-->
+                <!--</label>-->
+
+                <!--<label class="tabbar__item tabbar&#45;&#45;material__item" onclick="counterparties_view('DATE_CREATE','DESC','0')"  id="all_counterparties">-->
+                <!--<input type="radio" name="tabbar-material-a">-->
+                <!--<button class="tabbar__button tabbar&#45;&#45;material__button">-->
+                <!--Контрагенты-->
+                <!--</button>-->
+                <!--</label>-->
+
+                <!--<label class="tabbar__item tabbar&#45;&#45;material__item" onclick="leads_view('DATE_CREATE','DESC')"  id="all_leads">-->
+                <!--<input type="radio" name="tabbar-material-a">-->
+                <!--<button class="tabbar__button tabbar&#45;&#45;material__button">-->
+                <!--Лиды-->
+                <!--</button>-->
+                <!--</label>-->
+
+                <!--<label class="tabbar__item tabbar&#45;&#45;material__item">-->
+                <!--<input type="radio" name="tabbar-material-a">-->
+                <!--<button class="tabbar__button tabbar&#45;&#45;material__button">-->
+                <!--Games-->
+                <!--</button>-->
+                <!--</label>-->
+                <!--</div>-->
+            </div>
+        </div>
+    </div>
+
+    <div class="row" id="app">
+        <div style="width: 100%" class="row">
+
+            <div class="col-md-12" id="main_app">
+                <div id="maps">
+                    <iframe src="https://bxapp.megamix.ru/map/core/public/" width="100%" height="830px">
+                        Ваш браузер не поддерживает плавающие фреймы!
+                    </iframe>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+ Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+////отображение формы аутентификации
+//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+////POST запрос аутентификации на сайте
+//Route::post('login', 'Auth\LoginController@login');
+////POST запрос на выход из системы (логаут)
+//Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+//
+///**
+// * Маршруты регистрации...
+// */
+//
+////страница с формой Laravel регистрации пользователей
+//Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+////POST запрос регистрации на сайте
+//Route::post('register', 'Auth\RegisterController@register');
+//
+///**
+// * URL для сброса пароля...
+// */
+//
+////POST запрос для отправки email письма пользователю для сброса пароля
+//Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+////ссылка для сброса пароля (можно размещать в письме)
+//Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+////страница с формой для сброса пароля
+//Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+////POST запрос для сброса старого и установки нового пароля
+//Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('home', function () {
+    return view('home');
+})->middleware('auth');
+
+
+//Route::get('/counterpartiesView', 'CounterpartiesController@index')->name('megacrm\counterpartiesView');
+
+Route::get('/counterpartiesView', [
+    'middleware' => 'auth',
+    'uses' => 'CounterpartiesController@index'
+]);
